d745fa68453c1b69f1db599cd9c20a6e229aa2e7
christianmonfiston <109248449+christianmonfiston@users.noreply.github.com>
2024-01-30 19:05:55 -0500

    Making some changes to the pages

--- screens/Signup.js
+++ screens/Signup.js
@@ -7,71 +7,85 @@ import { Pressable } from "react-native";
 import { Image } from "react-native";
 import { TouchableOpacity } from "react-native";
 import { TouchableWithoutFeedback } from "react-native";
+import { KeyboardAvoidingView } from "react-native";
+import { Keyboard } from "react-native";
 const Signup = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <View style={styles.title}>
-          <Image
-            style={styles.image}
-            source={require("../images/WeatherIcon-1.png")}
-          />
-          <View style={styles.subtitle}>
-            <Text style={styles.title}>Create your account </Text>
-            <Text style={styles.titleTwo}>!</Text>
-          </View>
-        </View>
-        <View style={styles.userDetailsContainer}>
-          <View style={styles.userNameBox}>
-            <TextInput
-              placeholder="    Username"
-              placeholderTextColor={"white"}
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.container}>
+          <View style={styles.title}>
+            <Image
+              style={styles.image}
+              source={require("../images/WeatherIcon-1.png")}
             />
+            <View style={styles.subtitle}>
+              <Text style={styles.title}>Create your account </Text>
+              <Text style={styles.titleTwo}>!</Text>
+            </View>
           </View>
-          <View style={styles.emailBox}>
-            <TextInput placeholder="    Email" placeholderTextColor={"white"} />
+          <View style={styles.userDetailsContainer}>
+            <View>
+              <TextInput
+                placeholder="    Username"
+                placeholderTextColor={"white"}
+                style={styles.userNameBox}
+              />
+            </View>
+            <View>
+              <TextInput
+                placeholder="    Email"
+                placeholderTextColor={"white"}
+                keyboardType="email-address"
+                style={styles.emailBox}
+              />
+            </View>
+            <View>
+              <TextInput
+                placeholder="    Password"
+                placeholderTextColor={"white"}
+                selectionColor={"white"}
+                cursorColor={"white"}
+                style={styles.passwordBox}
+              />
+            </View>
           </View>
-          <View style={styles.passwordBox}>
-            <TextInput
-              placeholder="    Password"
-              placeholderTextColor={"white"}
-              selectionColor={"white"}
-            />
+          <View>
+            <TouchableOpacity style={styles.button}>
+              <Pressable style={styles.button}>
+                <Text style={{ color: "black", fontSize: 15 }}> Sign up </Text>
+              </Pressable>
+            </TouchableOpacity>
+          </View>
+          <View>
+            <Text
+              style={{
+                color: "white",
+                fontSize: 15,
+                justifyContent: "center",
+                alignContent: "center",
+                alignItems: "center",
+                alignSelf: "center",
+              }}
+            >
+              By continuing, I accept Cloudly's
+            </Text>
+            <Text
+              style={{
+                color: "#7B71EC",
+                fontSize: 15,
+                justifyContent: "center",
+                alignContent: "center",
+                alignItems: "center",
+                alignSelf: "center",
+              }}
+            >
+              Terms of Service and Privacy Policy
+            </Text>
           </View>
         </View>
-        <View>
-          <Pressable style={styles.button}>
-            <Text style={{ color: "black", fontSize: 15 }}> Sign up </Text>
-          </Pressable>
-        </View>
-        <View>
-          <Text
-            style={{
-              color: "white",
-              fontSize: 15,
-              justifyContent: "center",
-              alignContent: "center",
-              alignItems: "center",
-              alignSelf: "center",
-            }}
-          >
-            By continuing, I accept Cloudly's
-          </Text>
-          <Text
-            style={{
-              color: "#7B71EC",
-              fontSize: 15,
-              justifyContent: "center",
-              alignContent: "center",
-              alignItems: "center",
-              alignSelf: "center",
-            }}
-          >
-            Terms of Service and Privacy Policy
-          </Text>
-        </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -112,6 +126,8 @@ const styles = StyleSheet.create({
     marginBottom: 25,
     backgroundColor: "#383567",
     marginTop: 25,
+    color: "white",
+    fontSize: 15,
   },
   emailBox: {
     borderWidth: 2,
@@ -121,6 +137,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     justifyContent: "center",
     marginBottom: 25,
+    fontSize: 15,
     backgroundColor: "#383567",
   },
   passwordBox: {
@@ -132,6 +149,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     backgroundColor: "#383567",
     marginBottom: 20,
+    fontSize: 15,
   },
 
   button: {
